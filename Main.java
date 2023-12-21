741db18cdde1bf43f6213d6cb51443cdca315dc4
Blouinbishop <131938627+Blouinbishop@users.noreply.github.com>
2023-12-20 22:26:52 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class Main {
+
+	@SuppressWarnings("deprecation")
+	public static void main(String[] args) {
+		
+		 JFrame f = new WordEdit();
+		    f.show();
+
+	}
+
+}
